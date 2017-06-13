11feca50026e6b89c4608c0b798905df23f15325
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-06-13 11:13:14 -0300

    Preparing ground for MATLAB

--- labs/lab2/main.m
+++ labs/lab2/main.m
@@ -2,15 +2,19 @@ function main()
 % Função principal do MATLAB
 
 % Abrir o Arduino
-arduino = serial(input('What port? ', 's'), 'BaudRate', 9600);
+arduino = serial(input('Qual porta? ', 's'), 'BaudRate', 9600);
 fopen(arduino);
 stuff = [ ];
 
 % Ler os valores
-for i = 1:1000
-    line = fgetl(arduino);
-    fprintf('%s\n', line);
-end
+userInput = input('Qual filtro e quantas medidas? ', 's');
+[filterKind howMany] = sscanf(userInput, '%s %f');
+fprintf(arduino, '%s\n', userInput);
+line = fgetl(arduino);
+fprintf('%s\n', line);
 
 % Fechar o Arduino
 fclose(arduino);
+
+% TODO Implement plot functions
+% TODO Pick current filter
--- labs/lab2/sensor/sensor.ino
+++ labs/lab2/sensor/sensor.ino
@@ -1,9 +1,77 @@
+#define LED 13
+#define MEAN_FILTER 1
+#define WINDOW_FILTER 2
+#define EXP_FILTER 3
+#define TERMOPAR (A0)
+#define LM35 (A5)
+
+int HOW_MANY = -1;
+
 void setup()
 {
+    pinMode(LED, OUTPUT);
+    pinMode(TERMOPAR, INPUT);
+    pinMode(LM35, INPUT);
     Serial.begin(9600);
 }
 
 void loop()
 {
-    Serial.println(millis());
+
+}
+
+void serialEvent()
+{
+    String inlet = Serial.readString();
+
+    if (inlet.startsWith("media")) {
+        HOW_MANY = inlet.substring(5).toInt();
+        readWithMean();
+    }
+    else if (inlet.startsWith("movel")) {
+        HOW_MANY = inlet.substring(5).toInt();
+        readWithWindowed();
+    }
+    else if (inlet.startsWith("exp")) {
+        HOW_MANY = inlet.substring(3).toInt();
+        readWithExp();
+    }
+}
+
+// TODO Implement filters
+void readWithMean()
+{
+    for (int i = 0; i < HOW_MANY; ++i)
+    {
+        Serial.print(millis());
+        Serial.print(" ");
+        Serial.print(analogRead(TERMOPAR));
+        Serial.print(" ");
+        Seria.print(analogRead(LM35));
+        Serial.print(";");
+    }
+
+    Serial.println();
+}
+
+void readWithWindowed()
+{
+    for (int i = 0; i < HOW_MANY; ++i)
+    {
+        Serial.print(millis());
+        Serial.print(" ");
+    }
+
+    Serial.println();
+}
+
+void readWithExp()
+{
+    for (int i = 0; i < HOW_MANY; ++i)
+    {
+        Serial.print(millis());
+        Serial.print(" ");
+    }
+
+    Serial.println();
 }
