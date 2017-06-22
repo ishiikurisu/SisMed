bfdf4c11ae200c9d021f870e128e55cb81471a3e
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-06-22 15:01:06 -0300

    Adding filters to Arduino

--- labs/lab2/sensor/sensor.ino
+++ labs/lab2/sensor/sensor.ino
@@ -1,6 +1,12 @@
+#include "./filters.h"
 #define LED 13
 #define TERMOPAR (A0)
 #define LM35 (A5)
+
+MEAN_FILTER mean_filter;
+WINDOW_FILTER window_filter(10);
+EXP_FILTER exp_filter(0.7);
+double lm35_measure;
 
 void setup()
 {
@@ -12,9 +18,16 @@ void setup()
 
 void loop()
 {
-    float lm35_measure = (4 * analogRead(LM35) * 100.0) / 1024;
+    lm35_measure = (4 * analogRead(LM35) * 100.0) / 1024;
+    mean_filter.add(lm35_measure);
+    window_filter.add(lm35_measure);
+    exp_filter.add(lm35_measure);
     Serial.print(millis());
-    Serial.print(" ");
-    Serial.println(lm35_measure);
+    Serial.print("\t");
+    Serial.print(mean_filter.get());
+    Serial.print("\t");
+    Serial.print(window_filter.get());
+    Serial.print("\t");
+    Serial.println(exp_filter.get());
     delay(500);
 }
