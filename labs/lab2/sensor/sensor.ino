c670c6876a4303f6b47a71d80b8e7be5c262df2d
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-06-27 21:41:56 -0300

    Uploading changes made during presentation

--- labs/lab2/sensor/sensor.ino
+++ labs/lab2/sensor/sensor.ino
@@ -1,19 +1,32 @@
 #include "./filters.h"
 #define LED 13
 #define TERMOPAR (A0)
-#define LM35 (A5)
+#define LM35 (A4)
 
 /* ######################
    # PHYSICAL FUNCTIONS #
    ###################### */
 
 // Convert the voltage read from a K type thermocouple to celsius degrees
-// based on the reading of a LM35 sensor in celsius.
-double convert_to_temperature(double thermocouple, double ref_temp)
+// based on the reading of a LM35 sensor in volts.
+double convert_to_temperature(double termopar, double lm35)
 {
-    return (thermocouple * 0.08) - ref_temp;
-}
+   double lm35_1 = lm35 * 100;
+   double lm35_2 = lm35_1 * lm35_1;
+   double lm35_3 = lm35_2 * lm35_1;
+   double lm35_4 = lm35_3 * lm35_1;
 
+   // Polinomio gerado pela interpolação da tebela disponível em: http://sensorestemperatura.com.br/Fotos_pdf/TABELA%20K%201.pdf
+   double cold_voltage = 0.001455643972886 + 0.039088035432556 * lm35_1 + 0.000038064028883 * lm35_2 - 0.000000240144827 * lm35_3 + 0.000000000449283 * lm35_4;
+
+   double termopar_1 = 1000 * termopar/500 + cold_voltage; // unidade em milivolts
+   double termopar_2 = termopar_1 * termopar_1;
+   double termopar_3 = termopar_2 * termopar_1;
+   double termopar_4 = termopar_3 * termopar_1;
+   double termopar_5 = termopar_4 * termopar_1;
+
+   return -0.037834437781076 + 25.552551420040750 * termopar_1 - 0.570291877969017 * termopar_2 + 0.088906098929135 * termopar_3 - 0.004173887396488 * termopar_4 + 0.000006005013885 * termopar_5;
+}
 
 /* ################
    # MAIN PROGRAM #
@@ -46,8 +59,9 @@ void setup()
 void loop()
 {
     // Measuring
-    lm35_measure = (5 * analogRead(LM35) * 100.0) / 1024;
-    thermocouple_measure = convert_to_temperature(analogRead(TERMOPAR), lm35_measure);
+    lm35_measure = analogRead(LM35) * 5.0 / 1024;
+    thermocouple_measure = convert_to_temperature(analogRead(TERMOPAR)*5.0/1024/50, lm35_measure);
+    lm35_measure = lm35_measure * 100;
 
     // Filtering
     lm35_filter.add(lm35_measure);
